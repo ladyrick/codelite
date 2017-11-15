5024b8f1552f02a60e053cf70eb2dbe64fb227fd
liuhong12th <liuhong12th@163.com>
2017-11-15 23:19:33 +0800

    commit

--- test/main.cpp
+++ test/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <string>
 #include <gtest/gtest.h>
 
 using namespace std;
 
-unsigned int Factorial(unsigned int number) {
+unsigned long long Factorial(unsigned int number) {
+    if (number <= 1) {
+        return 1;
+    }
+    if (number > 20) {
+        throw ("input number must be less than 21.");
+    }
     return number <= 1 ? 1 : Factorial(number - 1) * number;
 }
 
-TEST(FooTest, HandleNoneZeroInput)
-{
+TEST(FooTest, HandleNoneZeroInput) {
     EXPECT_EQ(Factorial(0), 1);
     EXPECT_EQ(Factorial(1), 1);
     EXPECT_EQ(Factorial(2), 2);
@@ -22,18 +28,18 @@ TEST(FooTest, HandleNoneZeroInput)
     EXPECT_EQ(Factorial(10), 3628800);
     EXPECT_EQ(Factorial(11), 39916800);
     EXPECT_EQ(Factorial(12), 479001600);
-    EXPECT_EQ(Factorial(13), 1932053504);
-    EXPECT_EQ(Factorial(14), 27048749056);
-    EXPECT_EQ(Factorial(15), 405731235840);
-    EXPECT_EQ(Factorial(16), 6491699773440);
-    EXPECT_EQ(Factorial(17), 110358896148480);
-    EXPECT_EQ(Factorial(18), 1986460130672640);
-    EXPECT_EQ(Factorial(19), 37742742482780160);
-    EXPECT_EQ(Factorial(20), 754854849655603200);
+    EXPECT_EQ(Factorial(13), 6227020800);
+    EXPECT_EQ(Factorial(14), 87178291200);
+    EXPECT_EQ(Factorial(15), 1307674368000);
+    EXPECT_EQ(Factorial(16), 20922789888000);
+    EXPECT_EQ(Factorial(17), 355687428096000);
+    EXPECT_EQ(Factorial(18), 6402373705728000);
+    EXPECT_EQ(Factorial(19), 121645100408832000);
+    EXPECT_EQ(Factorial(20), 2432902008176640000);
+    EXPECT_THROW(Factorial(21), string);
 }
 
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-
 }
