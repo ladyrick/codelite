6518963985cbf5a33b158830ee9eea942e37dad4
liuhong12th <liuhong12th@163.com>
2017-11-17 16:38:34 +0800

    commit

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <gtest/gtest.h>
 
 using namespace std;
 
-int main() {
-    string s1 = "123";
-    string s2 = move(s1);
-    s1[1] = 'a';
-    cout << s1;
+int test(int n) {
+    return n;
+}
+
+TEST(a, b) {
+    EXPECT_EQ(test(1), 1);
+    EXPECT_EQ(test(2), 2);
+    EXPECT_EQ(test(3), 3);
+}
+
+int main(int argc, char **argv) {
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> v;
+    copy(a, a + 10, back_inserter(v));
+    copy(v.begin(), v.end(), ostream_iterator<int>(cout, "\n"));
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+    return 0;
 }
