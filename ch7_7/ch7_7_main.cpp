ef60dd6c987c6bb7229190e4c550850523d6133d
liuhong12th <liuhong12th@163.com>
2017-11-18 23:20:12 +0800

    commit

--- ch7_7/ch7_7_main.cpp
+++ ch7_7/ch7_7_main.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-void e1(){
+void e1() {
     bitset<32> b(123);
-    bitset<32> c("23223222322223222223",7, 7, '2', '3');
+    bitset<32> c("2222222233322222", 10, '3', '2');
     cout << b << endl;
     cout << c << endl;
     cout << bitset<32>() << endl;
 }
 
-int main(){
+int main() {
     e1();
     return 0;
 }
