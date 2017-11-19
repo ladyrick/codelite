fd5085c50b1e9909e5eb4cfbfb21361cbf67582c
liuhong12th <liuhong12th@163.com>
2017-11-19 22:09:23 +0800

    commit

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,27 +1,53 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
-#include <gtest/gtest.h>
+#include <sstream>
 
 using namespace std;
 
-int test(int n) {
-    return n;
-}
+class cast2bits {
+private:
+    struct bits {
+        unsigned char b7 : 1;
+        unsigned char b6 : 1;
+        unsigned char b5 : 1;
+        unsigned char b4 : 1;
+        unsigned char b3 : 1;
+        unsigned char b2 : 1;
+        unsigned char b1 : 1;
+        unsigned char b0 : 1;
+    };
 
-TEST(a, b) { // NOLINT
-    EXPECT_EQ(test(1), 1);
-    EXPECT_EQ(test(2), 2);
-    EXPECT_EQ(test(3), 3);
-}
+public:
+    int length;
+    ostringstream os;
 
-int main(int argc, char **argv) {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> v;
-    copy(a, a + 10, back_inserter(v));
-    copy(v.begin(), v.end(), ostream_iterator<int>(cout, "\n"));
+    template<class T>
+    // 构造函数竟然可以用模板？！
+    explicit cast2bits(const T &input) {
+        const T *pTInput = &input;
+        auto *pBitsinput = (bits *) pTInput;
+        int n = sizeof(T) / sizeof(bits);
+        length = 9 * n - 1;
+        for (int i = n - 1; i >= 0; --i) {
+            os << (int) ((pBitsinput + i)->b0) << (int) ((pBitsinput + i)->b1) << (int) ((pBitsinput + i)->b2) << (int) ((pBitsinput + i)->b3) << (int) ((pBitsinput + i)->b4) << (int) ((pBitsinput + i)->b5) << (int) ((pBitsinput + i)->b6) << (int) ((pBitsinput + i)->b7) << ' ';
+        }
+    }
 
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    friend ostream &operator<<(ostream &os, const cast2bits &c) {
+        os << "M" << string((unsigned long long int) (c.length - 2), ' ') << "L" << endl;
+        os << c.os.str() << endl;
+        return os;
+    }
+};
+
+
+int main() {
+    cout << cast2bits(123) << endl;
+    cout << cast2bits((long long) 123) << endl;
+    cout << cast2bits(123.0f) << endl;
+    cout << cast2bits(123.0) << endl;
+    cout << cast2bits('3') << endl;
+    cout << cast2bits("123") << endl;
+    cout << cast2bits(string("123")) << endl;
+    return 0;
 }
