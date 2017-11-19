cf2699a465e606142099d34dff3d31eed50a63c6
liuhong12th <liuhong12th@163.com>
2017-11-19 23:07:33 +0800

    commit

--- ch7_7/ch7_7_main.cpp
+++ ch7_7/ch7_7_main.cpp
@@ -5,13 +5,19 @@ using namespace std;
 
 void e1() {
     bitset<32> b(123);
-    bitset<32> c("2222222233322222", 10, '3', '2');
+    bitset<32> c(string("10101010"),0, 3);
     cout << b << endl;
     cout << c << endl;
     cout << bitset<32>() << endl;
 }
 
+void e2(){
+    cout << bitset<32>().set().to_ulong() << endl;
+    cout << bitset<64>().set().to_ullong() << endl;
+}
+
 int main() {
     e1();
+    e2();
     return 0;
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <bitset>
 
 using namespace std;
 
@@ -16,24 +17,34 @@ private:
         unsigned char b1 : 1;
         unsigned char b0 : 1;
     };
-
-public:
     int length;
     ostringstream os;
+    ostringstream data;
 
+public:
     template<class T>
     // 构造函数竟然可以用模板？！
     explicit cast2bits(const T &input) {
+        data << input;
         const T *pTInput = &input;
         auto *pBitsinput = (bits *) pTInput;
         int n = sizeof(T) / sizeof(bits);
         length = 9 * n - 1;
         for (int i = n - 1; i >= 0; --i) {
-            os << (int) ((pBitsinput + i)->b0) << (int) ((pBitsinput + i)->b1) << (int) ((pBitsinput + i)->b2) << (int) ((pBitsinput + i)->b3) << (int) ((pBitsinput + i)->b4) << (int) ((pBitsinput + i)->b5) << (int) ((pBitsinput + i)->b6) << (int) ((pBitsinput + i)->b7) << ' ';
+            os << (int) ((pBitsinput + i)->b0)
+               << (int) ((pBitsinput + i)->b1)
+               << (int) ((pBitsinput + i)->b2)
+               << (int) ((pBitsinput + i)->b3)
+               << (int) ((pBitsinput + i)->b4)
+               << (int) ((pBitsinput + i)->b5)
+               << (int) ((pBitsinput + i)->b6)
+               << (int) ((pBitsinput + i)->b7)
+               << ' ';
         }
     }
 
     friend ostream &operator<<(ostream &os, const cast2bits &c) {
+        os << c.data.str() << endl;
         os << "M" << string((unsigned long long int) (c.length - 2), ' ') << "L" << endl;
         os << c.os.str() << endl;
         return os;
@@ -49,5 +60,6 @@ int main() {
     cout << cast2bits('3') << endl;
     cout << cast2bits("123") << endl;
     cout << cast2bits(string("123")) << endl;
+    cout << cast2bits(bitset<66>().set());
     return 0;
 }
